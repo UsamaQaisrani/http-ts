7f12d69a6424ad8423527dfeb6959a6e73340297
Muhammad <usamaqaisranifps@gmail.com>
2026-03-04 17:26:50 +0000

    added echo response on server

---
+++ constants.ts
@@ -0,0 +1,3 @@
+export const Constants = {
+  CRLF: "\r\n",
+}
--- main.ts
+++ main.ts
@@ -1,14 +1,43 @@
 import * as net from "net";
+import { Constants } from "./constants";
 
 const server = net.createServer((socket) => {
   socket.on("close", () => {
-    socket.end();
+    closeSocket(socket);
   });
 
   socket.on("data", (data) => {
-    console.log(data.toString());
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    respondToRequest(data, socket);
   });
 });
 
 server.listen(4221, "localhost");
+
+function closeSocket(socket: net.Socket) {
+  socket.end();
+}
+
+function respondToRequest(data: Buffer, socket: net.Socket) {
+  const reqParts = data.toString().split(Constants.CRLF);
+  const reqLine = reqParts[0];
+  const reqLineParts = reqLine.split(" ");
+  const method = reqLineParts[0];
+  const path = reqLineParts[1];
+  console.log(`Method: ${method}, Path: ${path}`);
+  if (path === "/") {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  }
+  else if (path.includes("/echo")) {
+    const content = path.split("/echo/")[1];
+    const requestLine = "HTTP/1.1 200 OK" + Constants.CRLF;
+    const headers = "Content-Type: text/plain" + Constants.CRLF;
+    const body = "Content-Length:" + content.length + Constants.CRLF + Constants.CRLF + content;
+    socket.write(requestLine + headers + body);
+  }
+  else if (path.includes("/user-agent")) {
+
+  }
+  else {
+    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+  }
+}
