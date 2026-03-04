a36a09f16bf9af373fa44d032aad0dfe3ae87e4e
Muhammad <usamaqaisranifps@gmail.com>
2026-03-04 15:57:34 +0000

    sending ok reponse

--- main.ts
+++ main.ts
@@ -4,6 +4,11 @@ const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
+
+  socket.on("data", (data) => {
+    console.log(data.toString());
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  });
 });
 
 server.listen(4221, "localhost");
