600952317fdf2e9e770e7ac4cf97e956ad4122bc
Muhammad <usamaqaisranifps@gmail.com>
2026-03-04 18:52:34 +0000

    reading file contents and sending in response body

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 import * as net from "net";
 import { Constants } from "./constants";
+import * as process from "process";
+import * as fs from "fs";
+import * as fPath from "path";
 
+const args = process.argv.slice(2);
+console.log("Arguments:", args);
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     closeSocket(socket);
@@ -42,6 +47,22 @@ function respondToRequest(data: Buffer, socket: net.Socket) {
     const headers = "Content-Type: text/plain" + Constants.CRLF;
     const body = "Content-Length:" + agent.length + Constants.CRLF + Constants.CRLF + agent;
     socket.write(requestLine + headers + body); 
+  }
+  else if (path.includes("/files")) {
+    const filePath = path.split("/files/")[1];
+    const fullFilePath = fPath.join(args[1], filePath);
+    if (fs.existsSync(fullFilePath)) {
+
+      const content = fs.readFileSync(fullFilePath);
+      console.log("File content:", content.toString());
+      const requestLine = "HTTP/1.1 200 OK" + Constants.CRLF;
+      const headers = "Content-Type: application/octet-stream" + Constants.CRLF;
+      const body = "Content-Length:" + content.length + Constants.CRLF + Constants.CRLF + content;
+      socket.write(requestLine + headers + body);
+    }
+    else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
   }
   else {
     socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
